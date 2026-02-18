499bb7811944132782e1d509e9684d35afda9167
Ganesh Naik <ganeshnaiklavoodya@gmail.com>
2026-02-18 23:48:46 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config={
+  theme:{
+    gridtemplateColumns:{
+      'auto':'repeat(auto-fit,minmax(200px,1fr))'
+
+    },
+    fontFamily:{
+      Outfit:["Outfit","sans-serif"],
+    },
+    colors:
+    {
+      lightHover:'#fcf4ff',
+      darkHover:'#2a00'
+    }
+  }
+}
